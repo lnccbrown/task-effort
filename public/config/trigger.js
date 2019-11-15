a6698bfe49e417860373542d3ebded88a8720bcf
Rashi Dhar <rdhar1997@gmail.com>
2019-11-15 17:29:54 -0500

    feat: initial commit

---
+++ public/config/trigger.js
@@ -0,0 +1,21 @@
+// Event trigger settings - used in both the react app (renderer) and the electron app (main)
+const manufacturer = 'Teensyduino'
+const vendorId = '16c0'
+const productId = '0487'
+
+// NOTE - these event codes must match what is in public/config/trigger.js
+const eventCodes = {
+	fixation: 1,
+	evidence: 5,
+	show_earnings: 7,
+	test_connect: 32,
+	open_task: 18
+}
+
+// this is module.exports isntead of just exports as it is also imported into the electron app
+module.exports = {
+	manufacturer,
+	vendorId,
+	productId,
+	eventCodes
+}
