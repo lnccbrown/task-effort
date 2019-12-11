e4822aaafb5fa7ddd720ee6f3fdddac1f9fa1e0b
wasita mahaphanit <wasita@brown.edu>
2019-12-11 17:01:48 -0500

    fix: actually fix frameSpike start and end codes

--- src/trials/frameSpike.js
+++ src/trials/frameSpike.js
@@ -10,61 +10,67 @@ const canvasHTML = `<canvas width="${CANVAS_SIZE}" height="${CANVAS_SIZE}" id="j
   </canvas>`
 const fixationHTML = `<div id="fixation-dot" class="color-white"> </div>`
 
-  const frameSpike = (duration, effort, high_effort) => {
-    let stimulus = `<div class="effort-container">` + canvasHTML + fixationHTML + photodiodeGhostBox() + `</div>`
+const frameSpike = (duration, effort, high_effort) => {
+  let stimulus = `<div class="effort-container">` + canvasHTML + fixationHTML + photodiodeGhostBox() + `</div>`
 
-    const startCode = eventCodes.frameSpikeStart
-    const endCode = eventCodes.frameSpikeEnd
+  const startCode = eventCodes.frameSpikeStart
+  const endCode = eventCodes.frameSpikeEnd
 
-    return {
-      type: 'call_function',
-      async: true,
-      func: (done) => {
-        // add stimulus to the DOM
-        document.getElementById('jspsych-content').innerHTML = stimulus
-        // $('#jspsych-content').addClass('task-container')
+  return {
+    type: 'call_function',
+    async: true,
+    func: (done) => {
+      // add stimulus to the DOM
+      document.getElementById('jspsych-content').innerHTML = stimulus
+      // $('#jspsych-content').addClass('task-container')
 
       // set up canvas
       let canvas = document.querySelector('#jspsych-canvas');
       let ctx = canvas.getContext('2d');
 
-        const canvasDraw = () => {
-          // transparent background
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          var inflateBy;
-          var spikeHeight = [0, 0]
-          for (let i =0 ; i < 2; i++)
-          {
-            if (high_effort[i]) {
-                inflateBy = canvasSettings.inflateByHE
-            }
-            else {
-                inflateBy = canvasSettings.inflateByNHE
-            }
-
-            // how far should the spike be
-            var targetDist = 2 * inflateBy * (effort[i] - 1);
-            var balloonBaseHeight = canvasSettings.balloonBaseHeight + (2*canvasSettings.balloonRadius);
-            // distance of the spike from the top
-            spikeHeight[i] = effort[i]?(canvasSettings.frameDimensions[1] - balloonBaseHeight - targetDist - canvasSettings.spiketopHeight):0;
+      const canvasDraw = () => {
+        // transparent background
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        var inflateBy;
+        var spikeHeight = [0, 0]
+        for (let i =0 ; i < 2; i++)
+        {
+          if (high_effort[i]) {
+              inflateBy = canvasSettings.inflateByHE
           }
-          // var spikeRefHeight = canvasSettings.spikeRefHeight;
-          // var spikeHeight = [effort[0]?spikeRefHeight - effort[0]:0, effort[1]?spikeRefHeight - effort[1]:0];
-          drawFrame(ctx, canvasSettings.frameDimensions[0], canvasSettings.frameDimensions[1], canvasSettings.frameXpos[0], canvasSettings.frameYpos, canvasSettings.frameLinecolor, false)
-          drawSpike(ctx, canvasSettings.spikeWidth, spikeHeight[0], canvasSettings.spikeXpos[0], canvasSettings.spikeYpos, canvasSettings.frameLinecolor, canvasSettings.frameLinecolor, false)
+          else {
+              inflateBy = canvasSettings.inflateByNHE
+          }
 
-          drawFrame(ctx, canvasSettings.frameDimensions[0], canvasSettings.frameDimensions[1], canvasSettings.frameXpos[1], canvasSettings.frameYpos, canvasSettings.frameLinecolor, false)
-          drawSpike(ctx, canvasSettings.spikeWidth, spikeHeight[1], canvasSettings.spikeXpos[1], canvasSettings.spikeYpos, canvasSettings.frameLinecolor, canvasSettings.frameLinecolor, false)
+          // how far should the spike be
+          var targetDist = 2 * inflateBy * (effort[i] - 1);
+          var balloonBaseHeight = canvasSettings.balloonBaseHeight + (2 * canvasSettings.balloonRadius);
+          // distance of the spike from the top
+          spikeHeight[i] = effort[i] ? (canvasSettings.frameDimensions[1] - balloonBaseHeight - targetDist - canvasSettings.spiketopHeight) : 0;
+        }
+        // var spikeRefHeight = canvasSettings.spikeRefHeight;
+        // var spikeHeight = [effort[0]?spikeRefHeight - effort[0]:0, effort[1]?spikeRefHeight - effort[1]:0];
+        drawFrame(ctx, canvasSettings.frameDimensions[0], canvasSettings.frameDimensions[1], canvasSettings.frameXpos[0], canvasSettings.frameYpos, canvasSettings.frameLinecolor, false)
+        drawSpike(ctx, canvasSettings.spikeWidth, spikeHeight[0], canvasSettings.spikeXpos[0], canvasSettings.spikeYpos, canvasSettings.frameLinecolor, canvasSettings.frameLinecolor, false)
+
+        drawFrame(ctx, canvasSettings.frameDimensions[0], canvasSettings.frameDimensions[1], canvasSettings.frameXpos[1], canvasSettings.frameYpos, canvasSettings.frameLinecolor, false)
+        drawSpike(ctx, canvasSettings.spikeWidth, spikeHeight[1], canvasSettings.spikeXpos[1], canvasSettings.spikeYpos, canvasSettings.frameLinecolor, canvasSettings.frameLinecolor, false)
       }
 
       canvasDraw()
-      pdSpotEncode(startCode)
       setTimeout(
         () => {
-          done(pdSpotEncode(endCode))
+          done()
         },
         duration)
+      },
+      on_load: () => {
+        pdSpotEncode(startCode)
+      },
+      on_finish: (data) => {
+        pdSpotEncode(endCode)
+        data.code = [startCode, endCode]
+      }
     }
   }
-}
 export default frameSpike
