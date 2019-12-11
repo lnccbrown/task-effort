5bb2f6e59450f9b8bdd822afdd4c2d02122a8f30
wasita mahaphanit <wasita@brown.edu>
2019-12-11 17:14:07 -0500

    feat: rewProb start and end codes

--- src/trials/rewardProbability.js
+++ src/trials/rewardProbability.js
@@ -3,19 +3,25 @@ import { baseStimulus } from '../lib/markup/stimuli'
 import { photodiodeGhostBox, pdSpotEncode } from '../lib/markup/photodiode'
 
 const rewardProbability = (duration, probability) => {
-  const code = eventCodes.rewardProbability
+  const startCode = eventCodes.rewardProbabilityStart
+  const endCode = eventCodes.rewardProbabilityEnd
+
   return {
     type: 'html_keyboard_response',
     stimulus: '',
-    // prompt:  lang.prompt.continue.press,
     response_ends_trial: false,
     trial_duration: duration,
     on_start: (trial) => {
       trial.stimulus = baseStimulus(`<h1>${probability}</h1>`, true) +
       photodiodeGhostBox()
     },
-    on_load: () => pdSpotEncode(code),
-    on_finish: (data) => data.code = code
+    on_load: () => {
+      pdSpotEncode(startCode)
+    },
+    on_finish: (data) => {
+      pdSpotEncode(endCode)
+      data.code = [startCode, endCode]
+    }
   }
 }
 
