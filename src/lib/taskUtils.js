d1372bb41a5b0e6c29e604abfab8baeb7424ed56
wasita mahaphanit <wasita@brown.edu>
2019-12-17 11:32:59 -0500

    refactor: taskUtils.js

--- src/lib/taskUtils.js
+++ src/lib/taskUtils.js
@@ -3,23 +3,19 @@ import _ from 'lodash'
 
 // initialize starting conditions for each trial within a block
 const generateStartingOpts = (blockSettings) => {
-	if(blockSettings.is_practice)
-	{
+	if (blockSettings.is_practice) {
 		let startingOptions = blockSettings.probs.map( (c) => {
 			// Repeat each starting condition the same number of times
 			return _.range(blockSettings.repeats_per_condition).map( () => c )
 		})
-
 		return _.shuffle(_.flatten(startingOptions))
-	}
-	else
-	{
+	} else {
 		let opts = []
-		for (let i = 0; i < blockSettings.probs.length; i++){
-			for (let val in blockSettings.value){
-				for (let eff in blockSettings.effort){
-					opts.push({prob: blockSettings.probs[i], effort: [blockSettings.effort[eff],20], value: [blockSettings.value[val], 1], high_effort: [true, false], get_reward: [blockSettings.get_reward[i], blockSettings.get_reward[i]]})
-					if (blockSettings.counterbalance){
+		for (let i = 0; i < blockSettings.probs.length; i++) {
+			for (let val in blockSettings.value) {
+				for (let eff in blockSettings.effort) {
+					opts.push({prob : blockSettings.probs[i], effort: [blockSettings.effort[eff],20], value: [blockSettings.value[val], 1], high_effort: [true, false], get_reward: [blockSettings.get_reward[i], blockSettings.get_reward[i]]})
+					if (blockSettings.counterbalance) {
 						opts.push({prob: blockSettings.probs[i], effort: [20, blockSettings.effort[eff]], value: [1, blockSettings.value[val]], high_effort: [false, true], get_reward: [blockSettings.get_reward[i], blockSettings.get_reward[i]]});
 					}
 				}
