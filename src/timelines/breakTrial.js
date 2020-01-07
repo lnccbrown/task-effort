93eaf77ad2d1523caeb0d6fe8b5327109362d7d0
wasita mahaphanit <wasita@brown.edu>
2020-01-07 11:36:51 -0500

    fix: breakTrial() vs breakTrial
    coded breakTrial to be like taskTrial so it should be in same level of 'nestedness' when taskBlock is generated, but missed the ()
    this should hopefully fix the 'you need a stimulus in html_keyboard_response' errors i was receiving

--- src/timelines/breakTrial.js
+++ src/timelines/breakTrial.js
@@ -1,19 +1,14 @@
 // import trials
-import { jsPsych } from 'jspsych-react'
 import breakScreen from '../trials/breakScreen'
 
-const breakTrial = (blockSettings, blockDetails, opts) => {
-  // initialize trial details
-  let trialDetails = {
-    start_time: Date.now()
-  }
+const breakTrial = () => {
 
   let timeline = breakScreen()
 
-  return {
-		type: 'html_keyboard_response',
-		timeline: timeline
-	}
+    return {
+  		type: 'html_keyboard_response',
+  		timeline: timeline
+  	}
 }
 
 export default breakTrial
--- src/timelines/taskBlock.js
+++ src/timelines/taskBlock.js
@@ -20,15 +20,15 @@ const taskBlock = (blockSettings) => {
 
 	if (blockSettings.num_breaks > 0) {
 		let breakInterval = Math.floor(timeline.length / (blockSettings.num_breaks + 1))
-		// console.log('breakInterval ' + breakInterval)
-		// console.log('numBreaks ' + blockSettings.num_breaks)
+		console.log('breakInterval ' + breakInterval)
+		console.log('numBreaks ' + blockSettings.num_breaks)
 		for (let iBreak = 1; iBreak < blockSettings.num_breaks + 1; iBreak++) {
-			timeline.splice(iBreak*breakInterval, 0, breakTrial)
+			timeline.splice(iBreak*breakInterval, 0, breakTrial())
 	  }
 	}
 
-	// console.log('finalized timeline')
-	// console.log(timeline)
+	console.log('finalized timeline')
+	console.log(timeline)
 
   return {
 		type: 'html_keyboard_response',
