056bb538c401e5e45b951d5e7d5ef9a33f0c8242
wasita mahaphanit <wasita@brown.edu>
2020-01-13 13:14:40 -0500

    feat: 300 ms fixations

--- src/timelines/taskTrial.js
+++ src/timelines/taskTrial.js
@@ -19,20 +19,20 @@ const taskTrial = (blockSettings, blockDetails, opts) => {
     high_effort: [],
     start_time: Date.now()
   }
-  
+
   // timeline
   let timeline = [
     // show condition
-    fixation(500), // need ITI of ~500 btwn trials
+    fixation(300), // need ITI of ~500 btwn trials
     rewardProbability(500, blockSettings, opts),
     frameSpike(700, blockSettings, opts, trialDetails),
     costBenefits(1500, blockSettings, opts, trialDetails),
     choice(5000, blockSettings, opts),
     fixation(200),
     pressBalloon(25000, blockSettings),
-    fixation(500),
+    fixation(300),
     rewardFeedback(800, blockSettings, opts, trialDetails),
-    fixation(500),
+    fixation(300),
     cumulativeReward(800, blockSettings, blockDetails, opts, trialDetails),
     // end the trial
     trialEnd(trialDetails, 500)
