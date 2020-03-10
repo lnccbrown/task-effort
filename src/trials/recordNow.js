b80a5a7071d383db64efd7b6896fe64d8a2ec183
wasita mahaphanit <wasita@brown.edu>
2020-03-10 12:58:12 -0400

    feat: EEG recording screen indicates EEG file name

--- src/trials/recordNow.js
+++ src/trials/recordNow.js
@@ -1,19 +1,30 @@
 import { lang } from '../config/main'
 import { baseStimulus } from '../lib/markup/stimuli'
+import { jsPsych } from 'jspsych-react'
 
 const recordNow = () => {
-  let stimulus = baseStimulus(`
-    <div class='instructions'>
-    <h1>${lang.prompt.begin_recording}
-    </div>
-    `, true)
 
   return {
     type: 'html_keyboard_response',
-    stimulus: stimulus,
+    stimulus: '',
     prompt:  lang.prompt.confirm_recording,
-    response_ends_trial: true
+    response_ends_trial: true,
+
+    on_start: (trial) => {
+    let userId = jsPsych.data.get().select('patient_id').values
+    console.log(userId)
+
+    trial.stimulus = baseStimulus(`
+      <div class='instructions'>
+      <h1>${lang.prompt.begin_recording}<br>
+      ${lang.prompt.name_eeg_file}<br>
+      ${userId}
+      ${lang.prompt.effort_suffix}</h1>
+      </div>
+      `, true)
+    }
   }
+
 }
 
 export default recordNow
