55f203a2120caf373da394d698b1eb8e459b368e
wasita mahaphanit <wasita@brown.edu>
2020-06-08 09:03:48 -0400

    fix: conflict with userId

--- src/trials/userId.js
+++ src/trials/userId.js
@@ -2,21 +2,7 @@ import { lang, ONLINE, MTURK, PROLIFIC } from "../config/main";
 import { getUserId, getTurkUniqueId } from "../lib/utils";
 import { baseStimulus } from "../lib/markup/stimuli";
 
-<<<<<<< Updated upstream
 const userId = () => {
-  if (MTURK) {
-    return {
-      type: "html_keyboard_response",
-      stimulus: baseStimulus(`<h1>${lang.userid.set}</h1>`, true),
-      response_ends_trial: false,
-      trial_duration: 800,
-      on_finish: (data) => {
-        const uniqueId = getTurkUniqueId();
-        console.log(uniqueId);
-      },
-    };
-=======
-const userId = (blockSettings) => {
   if (ONLINE) {
     if (MTURK) {
       return {
@@ -42,7 +28,6 @@ const userId = (blockSettings) => {
         },
       };
     }
->>>>>>> Stashed changes
   } else {
     return {
       type: "survey_text",
