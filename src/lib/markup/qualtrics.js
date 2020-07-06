20aa0fb14c23573ecd5158116e7c4e895b533b67
wasita mahaphanit <wasita@brown.edu>
2020-07-06 12:57:25 -0400

    feat: washu irb qualtrics; feat: updated rules

--- src/lib/markup/qualtrics.js
+++ src/lib/markup/qualtrics.js
@@ -1,2 +1,2 @@
 export const qualtricsHTML =
-  '<div id="iframe"> <iframe width="900" height="900" src="https://brown.co1.qualtrics.com/jfe/form/SV_6xNgXiaN1eyozkh" frameborder="0" allowfullscreen ></iframe></div>';
+  '<div id="iframe"> <iframe width="900" height="900" src="https://brown.co1.qualtrics.com/jfe/form/SV_4TV6L8TgfBxQBbD" frameborder="0" allowfullscreen ></iframe></div>';
