f773d14cc32c5fa63d60824bc49ba48251cca577
wasita mahaphanit <wasita@brown.edu>
2020-08-10 11:58:53 -0400

    fix: update firebase build flow, update psiturk build flow; fix: remove online var in userid
    set REACT_APP_AT_HOME to true for both firebase & psiturk

--- src/trials/userId.js
+++ src/trials/userId.js
@@ -1,42 +1,40 @@
-import { lang, ONLINE, MTURK, PROLIFIC } from "../config/main";
+import { lang, MTURK, PROLIFIC } from "../config/main";
 import { getUserId, getTurkUniqueId, getProlificId } from "../lib/utils";
 import { baseStimulus } from "../lib/markup/stimuli";
 
 const userId = () => {
-  if (ONLINE) {
-    if (MTURK) {
-      return {
-        type: "html_keyboard_response",
-        stimulus: baseStimulus(`<h1>${lang.userid.set}</h1>`, true),
-        response_ends_trial: false,
-        trial_duration: 800,
-        on_finish: (data) => {
-          const uniqueId = getTurkUniqueId();
-          console.log(uniqueId);
-        },
-      };
-    } else if (PROLIFIC) {
-      return {
-        type: "html_keyboard_response",
-        stimulus: baseStimulus(`<h1>${lang.userid.get_prolific}</h1>`, true),
-        response_ends_trial: false,
-        trial_duration: 1000,
-        on_finish: (data) => {
-          getProlificId(data);
-          // console.log(uniqueId);
-        },
-        // previously: manual input of ID
-        // type: "survey_text",
-        // questions: [
-        //   {
-        //     prompt: baseStimulus(`<h1>${lang.userid.set_prolific}</h1>`, true),
-        //   },
-        // ],
-        // on_finish: (data) => {
-        //   getUserId(data);
-        // },
-      };
-    } // end prolific else if
+  if (MTURK) {
+    return {
+      type: "html_keyboard_response",
+      stimulus: baseStimulus(`<h1>${lang.userid.set}</h1>`, true),
+      response_ends_trial: false,
+      trial_duration: 800,
+      on_finish: (data) => {
+        const uniqueId = getTurkUniqueId();
+        console.log(uniqueId);
+      },
+    };
+  } else if (PROLIFIC) {
+    return {
+      type: "html_keyboard_response",
+      stimulus: baseStimulus(`<h1>${lang.userid.get_prolific}</h1>`, true),
+      response_ends_trial: false,
+      trial_duration: 1000,
+      on_finish: (data) => {
+        getProlificId(data);
+        // console.log(uniqueId);
+      },
+      // previously: manual input of ID
+      // type: "survey_text",
+      // questions: [
+      //   {
+      //     prompt: baseStimulus(`<h1>${lang.userid.set_prolific}</h1>`, true),
+      //   },
+      // ],
+      // on_finish: (data) => {
+      //   getUserId(data);
+      // },
+    };
   } else {
     return {
       type: "survey_text",
