8ee06215293a7d70e18fababa4be799d93af94f2
wasita mahaphanit <wasita@brown.edu>
2020-11-23 18:39:44 -0500

    feat: practiceBlockOver event code

--- src/config/trigger.js
+++ src/config/trigger.js
@@ -1,25 +1,26 @@
 // NOTE - these event codes must match what is in public/config/trigger.js
 const eventCodes = {
+  practiceBlockOver: 250,
   fixationStart: 1, // this is fixation dot onset
-  fixationEnd: 2, // this is fixation dot offset
+  // fixationEnd: 2, // this is fixation dot offset
   rewardProbabilityStart: 10, // reward probability (50% or 100%) onset
-  rewardProbabilityEnd: 11, // reward probability (50% of 100%) offset
+  // rewardProbabilityEnd: 11, // reward probability (50% of 100%) offset
   frameSpikeStart: 20, // frame and spike onset
-  frameSpikeEnd: 21, // frame and spike offset
+  // frameSpikeEnd: 21, // frame and spike offset
   costBenefitsStart: 30, // points and effort values for both options onset
-  costBenefitsEnd: 31, // points and effort values for both options offset
+  // costBenefitsEnd: 31, // points and effort values for both options offset
   choiceStart: 40, // frame + spike + balloons, when participant can choose blue or green
-  choiceEnd: 41, // frame + spike + balloons offset, once participant has made a choice
+  // choiceEnd: 41, // frame + spike + balloons offset, once participant has made a choice
   pressBalloonStart: 50, // once participant is able to pump the balloon by pressing a key
-  pressBalloonEnd: 51, // once the balloon has popped (once 25 secs from pressBalloonStart is over)
+  // pressBalloonEnd: 51, // once the balloon has popped (once 25 secs from pressBalloonStart is over)
   cumulativeRewardsStart: 60, // total points earned thus far onset
-  cumulativeRewardsEnd: 61, // total points earned thus far offse
+  // cumulativeRewardsEnd: 61, // total points earned thus far offse
   rewardFeedbackStart: 70, // trial reward feedback onset
-  rewardFeedbackEnd: 71, // trial reward feedback offset
+  // rewardFeedbackEnd: 71, // trial reward feedback offset
   showPaymentStart: 80, // end of task earnings screen onset
-  showPaymentEnd: 81, // end of task earnings screen offset
+  // showPaymentEnd: 81, // end of task earnings screen offset
   trialFinishStart: 90, // marks when trial starts (fixation onset) -- currently NOT in use
-  trialFinishEnd: 91, // marks when trial ends (reward feedback offset)
+  // trialFinishEnd: 91, // marks when trial ends (reward feedback offset)
   test_connect: 4,
   open_task: 5, // when the app is opened, send this code
 };
