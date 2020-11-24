96f2428399cf17d8ad008c30d12c5039a8e6f4ef
wasita mahaphanit <wasita@brown.edu>
2020-11-24 09:16:06 -0500

    chore: remove unused imports

--- src/trials/practiceAndMainBlockDivider.js
+++ src/trials/practiceAndMainBlockDivider.js
@@ -1,7 +1,6 @@
 import { eventCodes } from "../config/main";
 import { jitter50, removeCursor } from "../lib/utils";
-import { pdSpotEncode, photodiodeGhostBox } from "../lib/markup/photodiode";
-import { fixationHTML } from "../lib/markup/fixation";
+import { pdSpotEncode } from "../lib/markup/photodiode";
 import { jsPsych } from "jspsych-react";
 
 const practiceAndMainBlockDivider = (duration) => {
