fd8ea544137a770076a89622ebd672ee4ba98828
moblim <molimb@masonlive.gmu.edu>
2021-07-01 17:31:48 -0400

    instruction and break text additions. commented out framespike and jcostBenefits, added trialDetails to Choice.

--- src/timelines/taskBlock.js
+++ src/timelines/taskBlock.js
@@ -3,26 +3,26 @@ import breakTrial from './breakTrial'
 import { generateStartingOpts } from '../lib/taskUtils'
 
 const taskBlock = (blockSettings) => {
-  // initialize block
+	// initialize block
 	const startingOpts = generateStartingOpts(blockSettings)
 
-  const blockDetails = {
-	  block_earnings: 0.0,
+	const blockDetails = {
+		block_earnings: 0.0,
 		optimal_earnings: 0.0,
 		continue_block: true
 	}
 
 	// timeline = loop through trials
-	let timeline = startingOpts.map( (opt) => taskTrial(blockSettings, blockDetails, opt))
+	let timeline = startingOpts.map((opt) => taskTrial(blockSettings, blockDetails, opt))
 
 	if (blockSettings.num_breaks > 0) {
 		let breakInterval = Math.floor(timeline.length / (blockSettings.num_breaks + 1))
 		for (let iBreak = 1; iBreak < blockSettings.num_breaks + 1; iBreak++) {
-			timeline.splice(iBreak*breakInterval, 0, breakTrial())
-	  }
+			timeline.splice(iBreak * breakInterval, 0, breakTrial(iBreak))
+		}
 	}
 
-  return {
+	return {
 		type: 'html_keyboard_response',
 		timeline: timeline
 	}
--- src/timelines/taskTrial.js
+++ src/timelines/taskTrial.js
@@ -26,9 +26,9 @@ const taskTrial = (blockSettings, blockDetails, opts) => {
     // show condition
     fixation(300),
     rewardProbability(1000, blockSettings, opts, trialDetails),
-    frameSpike(700, blockSettings, opts, trialDetails),
-    costBenefits(1500, blockSettings, opts, trialDetails),
-    choice(5000, blockSettings, opts),
+    // frameSpike(700, blockSettings, opts, trialDetails),
+    // costBenefits(1500, blockSettings, opts, trialDetails),
+    choice(6000, blockSettings, opts, trialDetails),
     pressBalloon(25000, blockSettings, opts),
     fixation(500),
     rewardFeedback(1000, blockSettings, opts, trialDetails),
