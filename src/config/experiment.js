fe3e697392dcc7ac47b2b3e331f1e46978c6245a
Maria Isabel Restrepo <maria_restrepo@brown.edu>
2022-09-26 15:34:08 -0400

    chore: add experiment settings that get overloaded if environment variable is set

--- src/config/experiment.js
+++ src/config/experiment.js
@@ -15,12 +15,12 @@ exptBlock1.keys = ["q", "p", "m"];
 exptBlock1.get_reward = [true, true, true, false];
 exptBlock1.num_breaks = 3;
 
-// debug settings where there are only 2 trials in main task block
-//exptBlock1.probs = ["100%"];
-//exptBlock1.counterbalance = true;
-//exptBlock1.value = [3];
-//exptBlock1.effort = [5];
-//exptBlock1.keys = ["q", "p", "m"];
-//exptBlock1.get_reward = [true];
+// overwrite values for RemoveProbability experiments
+if(process.env.REACT_APP_exptOverload === "remove-probability"){
+    exptBlock1.probs = ["100%", "100%"];
+    exptBlock1.get_subset = false;
+    exptBlock1.get_reward = [true, true];
+    exptBlock1.num_breaks = 2;
+}
 
 export { exptBlock1 };
