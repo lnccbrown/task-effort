1bb808aecb53ded25639ee5d8295b992e499af0e
anvo2 <an_vo@brown.edu>
2022-10-14 13:27:19 -0400

    changed projectId, fixed dataURL

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const config = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   databaseURL: process.env.REACT_APP_databaseURL,
-  projectId:  process.env.REACT_APP_projectID || "no-firebase",
+  projectId:  process.env.REACT_APP_projectId || "no-firebase",
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
